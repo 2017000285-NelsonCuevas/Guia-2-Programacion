9511f16cae056e6757cd842e83e022c271cbb244
2017000285-NelsonCuevas <n.cuevas.5adb@gmail.com>
2026-04-16 12:02:43 -0600

    Add files via upload
    Ejercicio 3 Visual Programacion G2

---
+++ Ejercicio3.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("RANGO DE ESTUDIANTES");
+        Console.WriteLine("Ingrese una letra (A, B, C, D y F)");
+        char letra = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra)
+        {
+            case 'A':
+                Console.WriteLine("Escelente");
+                break;
+            case 'B':
+                Console.WriteLine("Bueno");
+                break;
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+            case 'D':
+                Console.WriteLine("deficiente");
+                break;
+            case 'F':
+                Console.WriteLine("Reprobado");
+                break;
+            default:
+                Console.WriteLine("Escriba una letra valida");
+                break;
+        }
+        }
+}
